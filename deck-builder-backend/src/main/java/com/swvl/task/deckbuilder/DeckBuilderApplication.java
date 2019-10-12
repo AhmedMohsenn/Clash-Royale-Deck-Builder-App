7eb1943621e0a94ee2204651a9ee9287f5efd1f6
AhmedMohsen <ahmed.mohsen-mohamed@valeo.com>
2019-10-12 16:48:36 +0200

    backend renaming

--- deck-builder-backend/src/main/java/com/swvl/task/deckbuilder/DeckBuilderApplication.java
+++ deck-builder-backend/src/main/java/com/swvl/task/deckbuilder/DeckBuilderApplication.java
@@ -1,11 +1,7 @@
 package com.swvl.task.deckbuilder;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.swvl.task.deckbuilder.entities.CardType;
-import com.swvl.task.deckbuilder.repositories.CardTypeRepository;
 
 @SpringBootApplication
 public class DeckBuilderApplication {
